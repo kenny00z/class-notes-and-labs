685714be1ca310e15cd3e15aa3bbafe389fa55d9
Jesús García <js.garcia.fernandez@gmail.com>
2022-11-21 15:25:57 +0100

    udpated async lab & some lessons

--- my-labs/Js/lab-es6-promises-master/javascript/index.js
+++ my-labs/Js/lab-es6-promises-master/javascript/index.js
@@ -6,34 +6,31 @@
 // Out of sync
   getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+
+    getInstruction("mashedPotatoes", 1, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+
+      getInstruction("mashedPotatoes", 2, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+     
+        getInstruction("mashedPotatoes", 3, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+
+          getInstruction("mashedPotatoes", 4, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+
+            getInstruction("mashedPotatoes", 5, (step6) => {
+              document.querySelector("#mashedPotatoes").innerHTML += `<li>${step6}</li>`;
+
+              document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+
+            }, (error) => console.log(error));
+          }, (error) => console.log(error));
+        }, (error) => console.log(error));
+      }, (error) => console.log(error));
+    }, (error) => console.log(error));
   }, (error) => console.log(error));
   
-
-
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
-  
-
-
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
-  
-
-
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
-  
-
-
-  getInstruction("mashedPotatoes", 4, (step5) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
-
-
 
 // Iteration 1 - using callbacks
 
@@ -73,15 +70,83 @@ obtainInstruction("steak", 0).then((paso) => {
 // Iteration 3 using async/await
 
 
-async function makeBroccoli(paso) {
+// async function makeBroccoli(obtainInstruction, step) {
 
-  let result = await obtainInstruction;
+//   const awaitFunction = await obtainInstruction;
 
+//   if (awaitFunction === "broccoli") {
+//     document.querySelector("#broccoli").innerHTML += `<li>${step}</li>`
+//   }
+// REPASAR POR QUE ESTABAS SIENDO ERRONEO //
+// }
+// makeBroccoli();
+
+async function makeBroccoli() {
+  const step0 = await obtainInstruction("broccoli", 0);
+  document.querySelector("#broccoli").innerHTML += `<li>${step0}</li>`;
+ 
+  // SEGUNDA MANERA 02
+  // await obtainInstruction("broccoli",0);
+  // document.querySelector("#broccoli").innerHTML += `<li>${broccoli[0]}</li>`;
+
+
+  // TERCERA MANERA 03
+  // document.querySelector("#broccoli").innerHTML += `<li>${await obtainInstruction('broccoli', 0)}</li>`;
+
+  const step1 = await obtainInstruction("broccoli", 1);
+  document.querySelector("#broccoli").innerHTML += `<li>${step1}</li>`;
+
+  const step2 = await obtainInstruction("broccoli", 2);
+  document.querySelector("#broccoli").innerHTML += `<li>${step2}</li>`;
+
+  const step3 = await obtainInstruction("broccoli", 3);
+  document.querySelector("#broccoli").innerHTML += `<li>${step3}</li>`;
+
+  const step4 = await obtainInstruction("broccoli", 4);
+  document.querySelector("#broccoli").innerHTML += `<li>${step4}</li>`;
+
+  const step5 = await obtainInstruction("broccoli", 5);
+  document.querySelector("#broccoli").innerHTML += `<li>${step5}</li>`;
+
+  const step6 = await obtainInstruction("broccoli", 6);
+  document.querySelector("#broccoli").innerHTML += `<li>${step6}</li>`;
+
+  ////// ESTE ULTIMO HAY QUE CREARLO ////////////////////////////////////////////
+  document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+////////////////////////////////////////////////////////////////////////////////
 }
 
 makeBroccoli();
+
+
+
 // --------------MIRAR POR INTERNET Y ACALARARME --------
 
 
 // Bonus 2 - Promise all
-// ...
+
+//TENEMOS 7 PASOS , HACER 7 VARIABLES PARA LLAMAR A CADA PASO
+
+const paso0 = obtainInstruction("brusselsSprouts", 0);
+const paso1 = obtainInstruction("brusselsSprouts", 1);
+const paso2 = obtainInstruction("brusselsSprouts", 2);
+const paso3 = obtainInstruction("brusselsSprouts", 3);
+const paso4 = obtainInstruction("brusselsSprouts", 4);
+const paso5 = obtainInstruction("brusselsSprouts", 5);
+const paso6 = obtainInstruction("brusselsSprouts", 6);
+const paso7 = obtainInstruction("brusselsSprouts", 7);
+
+Promise.all([paso0, paso1, paso2, paso3, paso4, paso5, paso6, paso7])
+  .then((valorDelArray) => {
+  valorDelArray.forEach((instruccion) => {
+    document.querySelector(
+      "#brusselsSprouts"
+    ).innerHTML += `<li>${instruccion}</li>`;
+  });
+
+   //AÑADIR TEXTO Y AÑADIR IMAGEN QUITANDO EL HIDDEN
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>Brussels Sprouts are ready!</li>`;
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+    
+}).catch((error) => console.log(error));
---
+++ section-03-Js/fecthApi/index.js
@@ -0,0 +1,87 @@
+
+
+
+//document.querySelector("#getJson").addEventListener("click", getText);
+
+//document.querySelector("#getHere").addEventListener("click", getText);
+
+// Funcion para llamar a un recurso interno que seria el doc TXT document "sample.txt"
+
+//PART 1 - crear función
+
+const getText = () => {
+    //PARTE 2 llamamos al metodo fecth y le pasamos la URL del recurso que queremos usar
+
+    fetch("sample.txt")
+        //PARTE 3 -recibimos info de la llamada por parte del fetch y lo que hacemos es dentro de la promesa llamamos a una epresion para poder manipular/limpiar la info que recibimos dentro del fetch, como esta info es texto tenemos que convertir este texto en una string con el .text()
+        .then((response) => response.text())
+        // .then((cleanText) => document.querySelector("#output").innerHTML += cleanText)
+        .then((cleanText) => document.querySelector("#output").innerText += cleanText)
+        .catch((error) => console.log(error));
+
+}
+
+
+
+document.querySelector("#getText").addEventListener("click", getText);
+
+const getJson = () => {
+    
+    fetch("users.json")
+        .then((response) => response.json())
+        .then((users) => {
+           
+            let userData = "";
+            users.forEach((user) => {
+                userData += `
+        <ul>
+        <li>${user.name}</li>
+        <li>${user.email}</li>
+        <li>${user.id}</li>
+        </ul>
+        `
+            });
+            document.querySelector("#output").innerHTML += userData;
+        })
+        .catch((error) => console.log(error));
+
+}
+
+document.querySelector("#getJson").addEventListener("click", getJson);
+
+
+//LLAMAR a una API externa xiquets
+
+
+const getExternalAPiInfo = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((res) => {
+        let externalApiInfo = "";
+        res.forEach((post) => {
+          externalApiInfo += `
+                <div class="card" style="width: 15rem">
+                  <img
+                      class="card-img-top"
+                      src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltde04169680d1e8d6/6333adfb110d52448841a252/GettyImages-1243560834.jpg"
+                      alt="A cool image about the Wu"
+                  />
+                  <div class="card-body">
+                      <h5 class="card-title">${post.title}</h5>
+                      <p class="card-text">
+                          ${post.body}
+                      </p>
+                  </div>
+                </div>
+                `;
+        });
+        document.querySelector("#output").innerHTML += externalApiInfo;
+      })
+      .catch((error) => console.log(error));
+  };
+  document
+    .querySelector("#getExternalAPiInfo")
+    .addEventListener("click", getExternalAPiInfo);
+  
+
+// getHere cambiada en los apuntes de diego por getExternalApiInfo.
